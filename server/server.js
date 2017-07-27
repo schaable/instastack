eaf642bb717fe905cf511005226127ede72b41b3
Luis Schaab <schaable@gmail.com>
2017-07-27 18:38:24 -0300

    Transpile server to ES5 and run with node

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Static files
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname));
 
 
 const imageList = [
