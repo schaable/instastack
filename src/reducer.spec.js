45df2c0f6c7a917a854110e3aaf8a086674f1bd7
Luis Schaab <schaable@gmail.com>
2017-07-28 14:58:49 -0300

    test

--- src/reducer.spec.js
+++ src/reducer.spec.js
@@ -6,6 +6,10 @@ it('should succeed as well!', () => {
   expect(1).toEqual(1);
 });
 
+it('should succeed as well!', () => {
+  expect(2).toEqual(2);
+});
+
 /* it('should succeed... oops!', () => {
   expect(0).toEqual('0');
 }); */
